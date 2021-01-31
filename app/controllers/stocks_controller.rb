8be964b82bcf65bf9bd001658e8cd1cc9eb5bb96
Shaher-11 <shahershamroukh@gmail.com>
2021-01-31 20:24:46 +0200

    added the stock controller

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,9 @@
+
+class StocksController < ApplicationController
+
+    def search
+      stock = Stock.new_lookup(params[:stock])
+      render json: stock
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
- 
+  get 'search_stock', to: 'stocks#search'
+
 end
