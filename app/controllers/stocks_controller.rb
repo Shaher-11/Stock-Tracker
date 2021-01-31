49fbda64a48a07d2d8dfea8fd402f8a01e643887
Shaher-11 <shahershamroukh@gmail.com>
2021-01-31 21:41:33 +0200

    added the rescue from the api iex

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,8 +2,18 @@
 class StocksController < ApplicationController
 
     def search
+     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
+      if @stock
       render 'users/my_portfolio'
+      else
+        flash[:alert] = "Please enter a valid symbol to the search"
+        redirect_to my_portfolio_path
+      end 
+     else 
+        flash[:alert] = "Please enter a symbol to the search"
+        redirect_to my_portfolio_path
+     end 
     end
 
 end
