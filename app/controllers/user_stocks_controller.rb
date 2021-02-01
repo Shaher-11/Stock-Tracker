115217e6fb18833dad371f7c30a7f6373ebe4e7d
Shaher-11 <shahershamroukh@gmail.com>
2021-02-01 17:12:13 +0200

    setup the user stock resources

---
+++ app/controllers/user_stocks_controller.rb
@@ -0,0 +1,3 @@
+class UserStocksController < ApplicationController
+    
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
+  validates :name, :ticker_symbol, presence: true
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
               endpoint: 'https://cloud.iexapis.com/v1')
