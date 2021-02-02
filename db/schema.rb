a4781fb021ec2a656fc1f2aebaa7c0905052366f
Shaher-11 <shahershamroukh@gmail.com>
2021-02-02 17:46:39 +0200

    modefied the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_143754) do
+ActiveRecord::Schema.define(version: 2021_02_02_152552) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_02_01_143754) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
