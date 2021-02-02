054fd021dbcffcb702627d4ad0066a96645601a9
Shaher-11 <shahershamroukh@gmail.com>
2021-02-02 10:06:01 +0200

    added the stock addign form

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,3 +1,13 @@
 class UserStocksController < ApplicationController
-    
-end
+    def create
+        stock = Stock.check_db(params[:ticker])
+        if stock.blank?
+        stock = Stock.new_lookup(params[:ticker])
+        stock.save
+        end 
+        @user_stock = UserStock.create(user: current_user, stock: stock)
+        flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+        redirect_to my_portfolio_path
+        
+    end 
+end 
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -12,4 +12,22 @@ class Stock < ApplicationRecord
          return nil
         end 
     end
+
+    def self.check_db(ticker_symbol)
+      where(ticker: ticker_symbol).first
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
