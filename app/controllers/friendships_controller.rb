6cef317e7463d043d6d6e31680a5b6a78e2d7ff4
Shaher-11 <shahershamroukh@gmail.com>
2021-02-03 20:13:45 +0200

    added styling to the table

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,18 @@
 class FriendshipsController < ApplicationController
 
   def create
-
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
-    friendship = current_user.friendships.where(friend_id: params [:id]).first
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
     flash[:notice] = "Stopped following"
     redirect_to my_friends_path
