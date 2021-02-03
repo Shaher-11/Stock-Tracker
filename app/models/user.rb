c988feec261ca1f1c483db481619e4f0eecb1224
Shaher-11 <shahershamroukh@gmail.com>
2021-02-03 18:43:00 +0200

    display friends search result

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,10 @@ class User < ApplicationRecord
     def self.matches(field_name, param)
       where("#{field_name} like ?", "%#{param}%")
     end
+
+    def except_current_user(users)
+      users.reject { |user| user.id == self.id }
+    end
 end
 
 
