dee8bd1dc1781b6bd6c35c0143be3ed8f9c99ba0
Shaher-11 <shahershamroukh@gmail.com>
2021-02-03 19:40:25 +0200

    added removing friends functionalty

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,6 +53,10 @@ class User < ApplicationRecord
     def except_current_user(users)
       users.reject { |user| user.id == self.id }
     end
+
+    def not_friends_with?(id_of_friend)
+      !self.friends.where(id: id_of_friend).exists?
+    end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
 end
