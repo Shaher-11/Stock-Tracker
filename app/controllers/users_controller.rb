ffc5caebe61f69313258bd6c15797209f26c7d18
Shaher-11 <shahershamroukh@gmail.com>
2021-02-04 09:58:12 +0200

    added the dispaly friends option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
-
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def search
     if params[:friend].present?
       @friends = User.search(params[:friend])
