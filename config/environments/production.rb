c98f2c0b7b7d2a1c976c92ed202d26c5d88bd17a
Shaher-11 <shahershamroukh@gmail.com>
2021-02-04 11:42:51 +0200

    api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,11 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
